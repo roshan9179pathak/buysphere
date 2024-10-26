4f2bfc02dc9d5b7533548b444aaed2d52b2a4b22
Roshan Pathak <roshan321pathak@gmail.com>
2024-10-26 18:12:12 +0530

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Head from 'next/head'
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -30,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+      <title>BuySphere</title>
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
